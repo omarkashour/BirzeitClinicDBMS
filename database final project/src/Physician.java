307ff36512e3a7d17ae4fcd01bdea805230061b3
Yaffa Issa <109853756+Yaffa9@users.noreply.github.com>
2023-06-29 11:07:51 +0000

    test

--- database final project/src/Physician.java
+++ database final project/src/Physician.java
@@ -33,10 +33,7 @@ public class Physician {
 	public String getFirst_name() {
 		return first_name;
 	}
-	
-	public void test(){
-		System.out.println("jj");
-	}
+
 
 	public void setFirst_name(String first_name) {
 		this.first_name = first_name;
